22ddc37fd6e369ab2f67b3dc0449c3f1ca41bd07
Aman <173646085+mkdiraman@users.noreply.github.com>
2025-09-17 21:09:16 -0700

    more pages added

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+
+window.addEventListener('scroll', function() {
+  // Adjust the 0.5 value for more/less parallax effect
+  document.body.style.backgroundPositionY = -(window.scrollY * 0.2) + 'px';
+});
