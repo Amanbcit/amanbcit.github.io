a0cf8c4dc5c1fb45a2d45cf8ca764fc0d2c0bb95
Aman <173646085+mkdiraman@users.noreply.github.com>
2025-09-19 15:46:49 -0700

    added ui/ux

---
+++ assets/scripts/about.js
@@ -0,0 +1,24 @@
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("skills-content");
+
+function opentab(event, tabname) {
+  var clickedTab = event.currentTarget;
+  var clickedContent = document.getElementById(tabname);
+
+  if (clickedTab.classList.contains("active-skill")) {
+    clickedTab.classList.remove("active-skill");
+    clickedContent.classList.remove("active-content");
+    return;
+  }
+
+  for (let tablink of tablinks) {
+    tablink.classList.remove("active-skill");
+  }
+  for (let tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-content");
+  }
+  clickedTab.classList.add("active-skill");
+  clickedContent.classList.add("active-content");
+}
