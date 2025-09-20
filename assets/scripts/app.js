a7797d367501adf5dc586325d1cbe713b7034238
Aman <173646085+mkdiraman@users.noreply.github.com>
2025-09-19 20:13:36 -0700

    home page design update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("scroll", function () {
-    // Adjust the 0.5 value for more/less parallax effect
-    document.body.style.backgroundPositionY = -(window.scrollY * 0.2) + "px";
-  });
+
+
+
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector('.header');
+  if (header) {
+    header.style.backgroundPosition = `center ${-window.scrollY * 0.5}px`;
+  }
 });
+
+//adding scroll detction for the navigation bar 
+
+let ticking = false;
+
+function updateNav() {
+  const nav = document.getElementById('nav-menu');
+  
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+  ticking = false;
+}
+
+window.addEventListener('scroll', function() {
+  if (!ticking) {
+    requestAnimationFrame(updateNav);
+    ticking = true;
+  }
+});
+
